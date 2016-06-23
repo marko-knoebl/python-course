dd8aa98aab725da50de148e6bdf90fc03a24fbdb
Marko Knöbl <marko.kn@gmail.com>
2016-06-23 19:26:03 +0200

    rest

--- einheit8/weather.py
+++ einheit8/weather.py
@@ -1,3 +1,13 @@
 # download the JSON data for London
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=76a06be0de6d6cb054d5110c719ef51b' % 
+import urllib2
+import json
+
+url = 'http://api.openweathermap.org/data/2.5/weather?q=Vienna&appid=76a06be0de6d6cb054d5110c719ef51b'
+
+weather_string = urllib2.urlopen(url).read()
+
+weather = json.loads(weather_string)
+
+print weather['clouds']['all']
+print weather['main']['temp']
